3722bbfb5e66c7559acf08721136990e12ef9a26
Chethan77 <chethanmc7799@gmail.com>
2021-09-17 17:49:40 +0530

    first commt

---
+++ BankLibrary.Tests/CalcInterestTests.cs
@@ -0,0 +1,19 @@
+﻿using BankingLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace BankLibrary.Tests
+{
+    [TestClass]
+    public class CalcInterestTests
+    {
+        [TestMethod]
+        public void Calculate_AcceptsPNR_Returns()
+        {
+            int ExpectedResult = 1000;
+            CalcInterest calc = new CalcInterest();
+            int ActualResult = calc.Calculate(10000, 10, 1);
+            Assert.AreEqual(ExpectedResult, ActualResult);
+;       }
+    }
+}
